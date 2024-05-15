96fd47bb413e4fe79074b85696c440207774567d
whh <huanhuan.wang01@hand-china.com>
2024-05-15 22:42:50 +0800

    fix: 递归数据劫持实现数组或对象的响应式未生效问题

--- 04-数据递归劫持/index.js
+++ 04-数据递归劫持/index.js
@@ -24,27 +24,26 @@ class Vue {
     }
 }
 function observe(data) {
-    let type = Object.prototype.toString.call(data);
-    // 如果是基本类型，就不返回
+    const type = Object.prototype.toString.call(data);
     if (type !== '[object Object]' && type !== '[object Array]') return;
     new Observer(data);
 }
+
 function defineReactive(obj, key, value) {
-    observe(obj[key])
-    // 实现data的响应式
+    observe(obj[key]);
     Object.defineProperty(obj, key, {
-        enumerable: true, // 可遍历
-        configurable: true, // 可删除
+        enumerable: true,
+        configurable: true,
         get: function reactiveGetter() {
-            console.log(`data的${key}取值`)
+            console.log(`读取${key}的值`)
             return value;
         },
         set: function reactiveSetter(val) {
             if (val === value) return;
-            console.log(`data的${key}发生了改变`)
+            console.log(`${key}的值发生了变化`);
             value = val;
         }
-    });
+    })
 }
 class Observer {
     constructor(data) {
@@ -52,7 +51,7 @@ class Observer {
     }
     walk(data) {
         const keys = Object.keys(data);
-        for (let i = 0; i < keys[i]; i++) {
+        for (let i = 0; i < keys.length; i++) {
             defineReactive(data, keys[i], data[keys[i]]);
         }
     }
