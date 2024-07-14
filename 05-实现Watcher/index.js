0ce2d5df508e3967f04f75eab0c86dcdba58b7e3
whh <huanhuan.wang01@hand-china.com>
2024-07-14 16:41:44 +0800

    feat: 实现vue中简单的watcher数据监听

---
+++ 05-实现Watcher/index.js
@@ -0,0 +1,105 @@
+class Vue {
+    constructor(options) {
+        this.$options = options;
+        this._data = options.data;
+        this.initData();
+    }
+    initData() {
+        let data = this._data;
+        // todo 暂时不考虑data是函数的情况
+        const keys = Object.keys(data);
+        for (let i = 0; i < keys.length; i++) {
+            Object.defineProperty(this, keys[i], {
+                enumerable: true, // 可遍历
+                configurable: true, // 可删除
+                get: function proxyGetter() {
+                    return data[keys[i]];
+                },
+                set: function proxySetter(value) {
+                    data[keys[i]] = value;
+                }
+            });
+        }
+        observe(data);
+        this.initWatch();
+    }
+    initWatch() {
+        let watch = this.$options.watch;
+        if (watch) {
+            const keys = Object.keys(watch);
+            for (let i = 0; i < keys.length; i++) {
+                this.$watch(keys[i], watch[keys[i]])
+            }
+        }
+    }
+    $watch(key, cb) {
+        new Watcher(this, key, cb)
+    }
+}
+function observe(data) {
+    const type = Object.prototype.toString.call(data);
+    if (type !== '[object Object]' && type !== '[object Array]') return;
+    new Observer(data);
+}
+
+function defineReactive(obj, key, value) {
+    observe(obj[key]);
+    let dep = new Dep();
+    Object.defineProperty(obj, key, {
+        enumerable: true,
+        configurable: true,
+        get: function reactiveGetter() {
+            dep.depend();
+            return value;
+        },
+        set: function reactiveSetter(val) {
+            if (val === value) return;
+            dep.notify();
+            value = val;
+        }
+    })
+}
+class Observer {
+    constructor(data) {
+        this.walk(data);
+    }
+    walk(data) {
+        const keys = Object.keys(data);
+        for (let i = 0; i < keys.length; i++) {
+            defineReactive(data, keys[i], data[keys[i]]);
+        }
+    }
+}
+class Dep {
+    constructor() {
+        this.subs = [];
+    }
+    depend() {
+        if (Dep.target) {
+            this.subs.push(Dep.target);
+        }
+    }
+    notify() {
+        this.subs.forEach((watcher) => {
+            watcher.run();
+        })
+    }
+}
+
+class Watcher {
+    constructor(vm, exp, cb) {
+        this.vm = vm;
+        this.exp = exp;
+        this.cb = cb;
+        this.get();
+    }
+    get() {
+        Dep.target = this;
+        this.vm[this.exp];
+        Dep.target = null;
+    }
+    run() {
+        // 绑定需要监听的对象的this
+        this.cb.call(this.vm);
+    }
+}
